bf870407c79b7a30be23f224d8b8aa2d5fffc19d
Connery MacRae <macrae.connery@gmail.com>
2024-10-15 22:35:35 -0400

    change pad

--- kanye-quotes/main.py
+++ kanye-quotes/main.py
@@ -12,7 +12,7 @@ def get_quote():
 
 window = Tk()
 window.title("Kanye Says...")
-window.config(padx=60, pady=60)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
