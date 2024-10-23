0ddc310f3b7ba841f879d852c0cd27223900ef3e
Connery MacRae <macrae.connery@gmail.com>
2024-10-23 13:14:35 -0400

    quizzler 2.0 complete

--- quizzler2.0/ui.py
+++ quizzler2.0/ui.py
@@ -1,15 +1,14 @@
 from tkinter import *
 from quiz_brain import QuizBrain
+import time
 
 THEME_COLOR = "#375362"
-# TRUE_IMG = PhotoImage(file="images/true.png")
-# FALSE_IMG = PhotoImage(file="images/false.png")
+
 
 class QuizInterface:
 
     def __init__(self, quiz_brain:QuizBrain):
         self.quiz = quiz_brain
-
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -17,7 +16,7 @@ class QuizInterface:
         true_image = PhotoImage(file="images/true.png")
         false_img = PhotoImage(file="images/false.png")
 
-        self.score_label = Label(text=f"Score: Current Score", bg=THEME_COLOR, fg="white")
+        self.score_label = Label(text=f"Score: {self.quiz.score}", bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0, column=1, pady=20)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
@@ -30,10 +29,10 @@ class QuizInterface:
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
-        self.true_button = Button(image=true_image, highlightthickness=0, highlightbackground=THEME_COLOR)
+        self.true_button = Button(image=true_image, highlightthickness=0, highlightbackground=THEME_COLOR, command=self.guess_true)
         self.true_button.grid(row=2, column=1, pady=20)
 
-        self.false_button = Button(image=false_img, highlightthickness=0, highlightbackground=THEME_COLOR)
+        self.false_button = Button(image=false_img, highlightthickness=0, highlightbackground=THEME_COLOR, command=self.guess_false)
         self.false_button.grid(row=2, column=0, pady=20)
 
         self.get_next_question()
@@ -41,5 +40,45 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text,
+                                   text=f"Quiz Complete!\nFinal Score: {self.quiz.score}/{len(self.quiz.question_list)}")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def guess_true(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+        # user_guess = "True"
+        # if self.quiz.check_answer(user_guess):
+        #     self.canvas.config(bg="green")
+        #     self.score_label.config(f"Score: {self.quiz.score}/{len(self.quiz.question_list)}")
+        # else:
+        #     self.canvas.config(bg="red")
+        # # self.score_label.config(text=f"Score: {current_score}/{len(self.quiz.question_list)}")
+        # self.window.after(1000, func=self.get_next_question)
+
+
+
+    def guess_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+        # user_guess = "False"
+        # if self.quiz.check_answer(user_guess):
+        #     self.canvas.config(bg="green")
+        #     self.score_label.config(text=f"Score: {self.quiz.score}/{len(self.quiz.question_list)}")
+        # else:
+        #     self.canvas.config(bg="red")
+        # # self.score_label.config(text=f"Score: {current_score}/{len(self.quiz.question_list)}")
+        # self.window.after(1000, func=self.get_next_question)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, func=self.get_next_question)
